1daea3d18050e9780272c7b19e35d82a6df1a17f
dpaak2@gamil.com <dpaak2@gamil.com>
2026-05-05 00:13:17 +0900

    test

--- 1st_week.py
+++ 1st_week.py
@@ -23,7 +23,7 @@ def get_current_prices_api(tickers):
     params          = {"markets": markets_param}    #사용할 parameter 
     #print(f"params ===> {params}")
     response        = requests.get(url, headers=headers, params=params) #api로 통신하여 값 조회
-    prices_data            = response.json()                                   #조회값 json으로 formatting
+    prices_data     = response.json()                                   #조회값 json으로 formatting
 
     ##json 데이터 출력 
     #print(f"prices_data : {prices_data}")
@@ -46,10 +46,43 @@ portfolio = {
 def analyze_portfolio(portfolio) : 
     tickers = list(portfolio.keys())
     #print('tickers => ',tickers)
+    #1.현재가 조회
     prices = get_current_prices_api(tickers)
     print(f'prices ==> {prices}')
+    #2.각 암호화폐별 분석  
+    # - 현재가 × 보유 수량 → 개별 가치 계산
+    # - `portfolio_analysis` 리스트에 코인명, 수량, 현재가, 가치 저장
+    # - 동시에 `total_value`(총합)를 누적
+    
 
+    portfolio_analysis  = []
+    total_value         = 0 
 
-    return None
+    for ticker, quantity in portfolio.items():
+        if ticker in prices:
+            current_price = prices[ticker]
+            value = current_price * quantity #현재가 × 보유 수량 → 개별 가치 계산
+            total_value += value #총합 누적
+            portfolio_analysis.append({
+                "코인": ticker.split('-')[1],  # 'KRW-BTC' -> 'BTC' 
+                "수량": quantity,
+                "현재가": current_price,
+                "가치": value
+            })
+    #3.비중계산
+    for item in portfolio_analysis:
+        item['비중'] = (item['가치'] / total_value) * 100 
+    
+    # print("="*80)
+    # print("포트폴리오 분석 결과")
+    # print("="*80)
+    # print(f"\n총 포트폴리오 가치: {total_value:,.0f}원\n")
+    # print("-"*80)
+    # print(f"{'코인':^10} {'수량':>15} {'현재가':>18} {'가치':>18} {'비중':>12}")
+    # print("-"*80)
+
+    return portfolio_analysis
 
 print('analyze_portfolio >> ', analyze_portfolio(portfolio))
+
+
